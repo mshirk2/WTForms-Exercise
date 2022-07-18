864e0e9b80d3f0aa0e7d3820e17ed136d95bb4eb
Michelle Shirk <michelle.l.shirk@gmail.com>
2022-07-18 19:25:18 -0400

    added forms

--- app.py
+++ app.py
@@ -35,6 +35,15 @@ def add_form():
     if form.validate_on_submit():
         name = form.name.data
         species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+
+        pet = Pet(name=name, species=species, photo_url=photo_url, age=age, notes=notes)
+
+        db.session.add(pet)
+        db.session.commit()
+
         flash(f"Added {name}")
         return redirect("/")
 
--- forms.py
+++ forms.py
@@ -1,8 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
+from wtforms.validators import InputRequired, Optional, URL, AnyOf, NumberRange
 
 class AddPetForm(FlaskForm):
     """Form for adding pet"""
 
-    name = StringField("Name")
-    species = StringField("Species")
+    name = StringField("Name", validators=[
+        InputRequired(message="Name is required")])
+    
+    species = StringField("Species", validators=[
+        InputRequired(message="Species is required"), 
+        AnyOf(values=dog cat rabbit, message="Species must be dog, cat or rabbit")])
+    
+    photo_url = StringField("Photo URL", validators=[
+        Optional(strip_whitespace=True), 
+        URL(message="Must be a valid URL")])
+    
+    age = IntegerField("Age", validators=[
+        Optional(strip_whitespace=True),
+        NumberRange(min=0, max=30, message="Age must be between 0-30")])
+   
+    notes = StringField("Notes", validators=[
+        Optional(strip_whitespace=True)])
